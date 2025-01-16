7812180790b76dc327e3299cd2db344dc86782f3
Niamh Driscoll <niamhmolson@outlook.com>
2025-01-16 14:30:13 -0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,52 @@
+def on_gesture_shake():
+    global Flip_coin
+    basic.show_leds("""
+        . # # # .
+        # . . . #
+        # . # . #
+        # . . . #
+        . # # # .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    """)
+    basic.pause(100)
+    basic.show_leds("""
+    . # # # .
+    # . # . #
+    # # # # #
+    # . # . #
+    . # # # .
+    """)
+    Flip_coin = randint(1, 2)
+    if Flip_coin == 1:
+        basic.show_leds("""
+            . # # # .
+            # . # . #
+            # # # # #
+            # . # . #
+            . # # # .
+            """)
+    else:
+        basic.show_leds("""
+            . # # # .
+            # . . . #
+            # . # . #
+            # . . . #
+            . # # # .
+            """)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+Flip_coin = 0
+basic.show_leds("""
+    . # # # .
+    # . . . #
+    # . # . #
+    # . . . #
+    . # # # .
+    """)
---
+++ main.ts
@@ -0,0 +1,53 @@
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . # . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+    basic.pause(100)
+    basic.showLeds(`
+    . # # # .
+    # . # . #
+    # # # # #
+    # . # . #
+    . # # # .
+    `)
+    Flip_coin = randint(1, 2)
+    if (Flip_coin == 1) {
+        basic.showLeds(`
+            . # # # .
+            # . # . #
+            # # # # #
+            # . # . #
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . # . #
+            # . . . #
+            . # # # .
+            `)
+    }
+    
+})
+let Flip_coin = 0
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . # . #
+    # . . . #
+    . # # # .
+    `)
